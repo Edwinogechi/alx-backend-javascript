bee7a9464edb6fcaed03a6b91176171cc601cb93
Edwinogechi <edwinogechi96@gmail.com>
2024-03-05 08:30:37 +0000

    Using spread syntax, concatenate 2 arrays and each character of a string

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,5 @@
 // Define a function named concatArrays
 export default function concatArrays(array1, array2, string) {
-	// Return a new array by concatenating array1, array2, and string
+  // Return a new array by concatenating array1, array2, and string
   return [...array1, ...array2, ...string];
 }
